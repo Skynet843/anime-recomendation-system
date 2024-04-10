9314b0b65d94f40d126b032174c58ff14112af92
Souvik Samanta <souviksamanta3103@gmail.com>
2024-04-10 11:20:23 +0530

    Content Based and Scrapping of Data Complete

--- .ipynb_checkpoints/mal_data_scrap-checkpoint.py
+++ .ipynb_checkpoints/mal_data_scrap-checkpoint.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 import re
 import pandas as pd
+import time
+import json
 
 
 def extract_info(text):
@@ -21,7 +23,7 @@ def extract_info(text):
 
 
 def firstStage(min_limit,max_limit):
-    anime_data=pd.DataFrame(columns=['name','type','episodes','mal_link'])
+    anime_data=pd.DataFrame(columns=['name','type','episodes','mal_id'])
     print("Fething Data......")
     for page in tqdm(range(min_limit,max_limit+1,50)):
         site_link=f"https://myanimelist.net/topanime.php?limit={page}"
@@ -35,13 +37,67 @@ def firstStage(min_limit,max_limit):
             details=item.select(".hoverinfo_trigger")[1]
             name=details.text
             mal_link=details.get('href')
+            id_pattern = r'/(\d+)/'
+            mal_id = re.search(id_pattern, mal_link).group(1)
             type,episodes=extract_info(item.select("td > div:nth-child(2) > div:nth-child(4)")[0].text)
-            anime_detail=pd.DataFrame({'name':[name],'type':[type],'episodes':[episodes],'mal_link':[mal_link]})
+            anime_detail=pd.DataFrame({'name':[name],'type':[type],'episodes':[episodes],'mal_id':[str(mal_id)]})
             anime_data = pd.concat([anime_data,anime_detail], ignore_index=True)
     anime_data.to_csv('anime_data.csv',chunksize=100,index=False)
     print("CSV File Save Successfully")
+
+import time
+def secondStage(data):
+    english_titles=[]
+    studios=[]
+    scores=[]
+    descriptions=[]
+    generes=[]
+    count=0
+    for site_id in tqdm(data['mal_id']):
+        site_link=f"https://api.jikan.moe/v4/anime/{site_id}"
+        time.sleep(0.5)
+        count=count+1
+        try:
+            r=requests.get(site_link)
+            while r.status_code==429:
+                time.sleep(5)
+                r=requests.get(site_link)
+            parsed_data = json.loads(r.text)['data']
+            english_title=parsed_data.get('title_english',None)
+            studio=[]
+            for studio_item in parsed_data.get('studios',None):
+                studio.append(studio_item.get('name',None))
+            score=parsed_data.get('score',None)
+            description=parsed_data.get('synopsis',None)
+            genere=[]
+            for genere_item in parsed_data.get('themes',None):
+                genere.append(genere_item.get('name',None))
+            for genere_item in parsed_data.get('demographics',None):
+                genere.append(genere_item.get('name',None))
+            for genere_item in parsed_data.get('genres',None):
+                genere.append(genere_item.get('name',None))
+            english_titles.append(english_title)
+            studios.append(studio)
+            scores.append(score)
+            generes.append(genere)
+            descriptions.append(description)
+        except:
+            english_titles.append(None)
+            studios.append(None)
+            scores.append(None)
+            generes.append(None)
+            descriptions.append(None)
+            print("BOOM BUM")
+    data['english_title'] = english_titles
+    data['studio'] = studios
+    data['score'] = scores
+    data['genere'] = generes
+    data['description']=descriptions
+    data.to_csv('anime_data_updated.csv',chunksize=100,index=False)
         
 if '__main__' == __name__ :
-    firstStage(0,50)
+    # firstStage(0,10000)
+    data=pd.read_csv('anime_data.csv')
+    secondStage(data)
         
     
